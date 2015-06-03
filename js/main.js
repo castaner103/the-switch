5384f05c23adba08f307960bdcc7b0e7cba811dd
erick castaneda <castaner103@gmail.com>
2015-06-02 21:00:17 -0400

    switch_branch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+$(document).ready(function(){
+	$('button').click(function(event){
+		event.preventDefault();
+		
+		$('.light').toggleClass('dark',"switch");
+			$('.on').toggleClass('off',"switch");
+		});					
 
 
+	$('button').click(function(event){
+		event.preventDefault();
+		if ($('.status').text() === "It's so bright in here!") {
+			$('.status').text("Who turned out the lights?");
+		}
+		else{
+			$('.status').text("It's so bright in here!");
+		}
+	});
+		
+
+});
